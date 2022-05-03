f327a33c87f1f6edeb0363d53fce8e1370ed472d
sofia <sofiavign@gmail.com>
2022-05-03 09:32:27 +0200

    delete vars

--- lib/sendgrid.ts
+++ lib/sendgrid.ts
@@ -1,8 +1,5 @@
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// sgMail.setApiKey(
-//   "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"
-// );
 
 export { sgMail };
